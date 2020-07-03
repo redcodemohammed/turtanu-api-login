e483aac929bbf181602a02bdeabca5e17278e9a2
ali <aljnabyl74@gmail.com>
2020-07-03 21:40:59 +0300

    First Commit

---
+++ untitled2/urls.py
@@ -0,0 +1,33 @@
+"""untitled2 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from apiLogin import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/accounts', include('apiLogin.urls')),
+
+    # path to djoser end points
+
+    #  path('auth/', include('djoser.urls')),
+    # path('auth/', include('djoser.urls.jwt')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('', views.hello.as_view(), name='hello'),
+    path('api/token/refresh', TokenRefreshView.as_view()),
+]
